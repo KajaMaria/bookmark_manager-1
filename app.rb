3a89697aa472adff4094fcee5d1f597cd015044e
Kaja <catcalledkaja@gmail.com>
2019-07-18 16:30:29 +0100

    adding finished 13

--- app.rb
+++ app.rb
@@ -28,5 +28,14 @@ enable :sessions, :method_override
     redirect '/bookmarks'
   end
 
+  get '/bookmarks/:id/edit' do
+    @bookmark = Bookmark.find(id: params[:id])
+    erb :'bookmarks/edit'
+  end
+  patch '/bookmarks/:id' do
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
+    redirect('/bookmarks')
+  end
+
   run! if app_file == $0
 end
